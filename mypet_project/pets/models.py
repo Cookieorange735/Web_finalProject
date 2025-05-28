dbc18749400f2953f6b10504318373069471d4a5
Li XinFeng <154742657+Mian-Tiao@users.noreply.github.com>
2025-05-28 22:59:29 +0800

    Add files via upload

--- mypet_project/pets/models.py
+++ mypet_project/pets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import math
 
 class Pet(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -15,3 +17,26 @@ class Pet(models.Model):
     def __str__(self):
         return f"{self.name} ({self.owner.username})"
 
+    def get_time_passed_minutes(self):
+        now = timezone.now()
+        delta = now - self.last_interaction
+        return int(delta.total_seconds() // 60)
+
+    # def update_status(self):
+    #     """根據時間差，更新飢餓、快樂、清潔值"""
+    #     minutes = self.get_time_passed_minutes()
+    #     if minutes <= 0:
+    #         return  # 沒過時間就不更新
+
+    #     # 每分鐘變動的數值（你可以自訂調整速度）
+    #     hunger_increase = minutes * 1
+    #     happiness_decrease = minutes * 0.5
+    #     cleanliness_decrease = minutes * 0.3
+
+    #     # 新值的計算
+    #     self.hunger = min(self.hunger + hunger_increase, 100)
+    #     self.happiness = max(self.happiness - math.ceil(happiness_decrease), 0)
+    #     self.cleanliness = max(self.cleanliness - math.ceil(cleanliness_decrease), 0)
+
+    #     self.last_interaction = timezone.now()
+    #     self.save()
--- mypet_project/pets/urls.py
+++ mypet_project/pets/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', views.register, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('adopt/', views.adopt_pet, name='adopt_pet'),
     path('pet/', views.view_pet, name='view_pet'),
